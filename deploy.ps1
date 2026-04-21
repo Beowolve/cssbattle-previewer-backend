215d74a9548cfccf697616f97e6d1eed4b52fb10
Andreas Ebner <beowolve@gmail.com>
2026-04-21 12:49:09 +0200

    Update deployment script memory allocation and Node.js version in package.json

--- deploy.ps1
+++ deploy.ps1
@@ -2,6 +2,7 @@ gcloud run deploy cssbattle-previewer-backend `
   --source . `
   --function renderPreview `
   --base-image google-22-full/nodejs22 `
+  --memory 2Gi `
   --region europe-west3 `
   --allow-unauthenticated `
   --allow-unauthenticated
